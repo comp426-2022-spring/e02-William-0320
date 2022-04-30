aa7f215c14a961795255ce4a2dfebec7d5331f0b
William-0320 <lisiyuan010320@hotmail.com>
2022-04-30 18:50:24 -0400

    Changes

--- src/App.js
+++ src/App.js
@@ -38,19 +38,19 @@ function App() {
     if(clear) setClear(false);
     // These variables correspond to the IDs of the input fields in your HTML form below
 // 1. Create other variables for assignments, exams, and engagement here
-    let currentA00 = document.querySelector('#a00').value
-    let currentA01 = document.querySelector('#a01').value
-    let currentA02 = document.querySelector('#a02').value
-    let currentA03 = document.querySelector('#a03').value
-    let currentA04 = document.querySelector('#a04').value
-    let currentA05 = document.querySelector('#a05').value
-    let currentA99 = document.querySelector('#a99').value
-    let currentQ1 = document.querySelector('#questionnaire01').value
-    let currentMidterm = document.querySelector('#midterm').value
-    let currentFinal = document.querySelector('#final').value
-    let currentCommits = document.querySelector('#commits').value
-    let currentE01 = document.querySelector('#e01').value
-    let currentE02 = document.querySelector('#e02').value
+    let currentA00 = document.querySelector('#a00').value || 0
+    let currentA01 = document.querySelector('#a01').value || 0
+    let currentA02 = document.querySelector('#a02').value || 0
+    let currentA03 = document.querySelector('#a03').value || 0
+    let currentA04 = document.querySelector('#a04').value || 0
+    let currentA05 = document.querySelector('#a05').value || 0
+    let currentA99 = document.querySelector('#a99').value || 0
+    let currentQ1 = document.querySelector('#questionnaire01').value || 0
+    let currentMidterm = document.querySelector('#midterm').value || 0
+    let currentFinal = document.querySelector('#final').value || 0
+    let currentCommits = document.querySelector('#commits').value || 0
+    let currentE01 = document.querySelector('#e01').value || 0
+    let currentE02 = document.querySelector('#e02').value || 0
     if(currentSum==='')
     return;
 // 2. This line is where you add the points all together. Add your other variable references here. Make sure that they match what is above and what is below in the HTML form
